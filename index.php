ab7bf9a6e5985c3f00df763a85b97de2ef3e956c
theoL <theo1007@outlook.fr>
2019-03-13 15:20:06 +0100

    Ajout de l'exo 8

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+  $addition = 3 + 4;
+  $multiplication = 5 * 20;
+  $division = 45 / 5;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+       echo $addition;
+      ?>
+    </p>
+    <p>
+      <?php
+       echo $multiplication;
+      ?>
+    </p>
+    <p>
+      <?php
+       echo $division;
+      ?>
+    </p>
+  </body>
+</html>
